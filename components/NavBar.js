30e478a9b32af9b591736503ed9d22e46a15ddf8
lhk3337 <lhk3337@naver.com>
2022-01-11 13:44:25 +0900

    add movie api

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Img from "next/image";
 import { useRouter } from "next/router";
 
 export default function NavBar() {
@@ -6,18 +7,39 @@ export default function NavBar() {
 
   return (
     <nav>
-      <Link href="/">
-        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
-      </Link>
+      <Img src="/vercel.svg" width={100} height={22} />
+      <div>
+        <Link href="/">
+          <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+        </Link>
+        <Link href="/about">
+          <a className={router.pathname === "/about" ? "active" : ""}>About</a>
+        </Link>
+      </div>
       <style jsx>{`
-        a {
-          text-decoration: none;
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        Img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
           color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
-
+const API_KEY = "1ca0919779771cad858efa04a2bc06c4";
 const Home = () => {
+  const [movies, setMovie] = useState();
+
+  useEffect(() => {
+    const fetchs = async () => {
+      const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
+      setMovie(results);
+    };
+    fetchs();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1 className="active">Home</h1> {/* 컬러가 노란색으로 바뀌지 않음 */}
+      {!movies && <h4>loading</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 };

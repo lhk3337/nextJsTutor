6c55191331fb8703bea24d7e8904557a1f615a4c
lhk3337 <lhk3337@naver.com>
2022-01-11 20:10:20 +0900

    img tag url 수정

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ results }) => {
 
       {results?.map((movie) => (
         <div key={movie.id} className="movie">
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}

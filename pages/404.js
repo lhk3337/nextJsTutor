b4b9867ebbe4aa7f6e89c6f24393523b89f33877
lhk3337 <lhk3337@naver.com>
2022-01-12 21:39:00 +0900

    notfound component add

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+export default function NotFound() {
+  return (
+    <div className="container">
+      <h1>What are u doing here?</h1>
+      <style jsx>
+        {`
+          .container {
+            margin-top: 300px;
+            display: flex;
+            font-size: 30px;
+            flex-direction: column;
+            align-items: center;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
